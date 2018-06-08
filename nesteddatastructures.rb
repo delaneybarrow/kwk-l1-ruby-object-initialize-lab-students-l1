bba548785d11d0fc95eb5be47e48224989d844d0
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-08 18:59:49 +0000

    automatically backed up by learn

--- nesteddatastructures.rb
+++ nesteddatastructures.rb
@@ -5,7 +5,7 @@
 group_name = {
   :delaney => ["sunflower", "all of the lights", "loving is easy"],
   :braelyn => ["mine", "bad at love", "mad world"],
-  :victoria => [""]
+  :victoria => ["life of the party", "september", "ain't gonna die tonight"]
 }
 puts group_name[:delaney][1]
 puts group_name[:braelyn][0]
